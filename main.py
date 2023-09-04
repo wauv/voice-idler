1938da5a52cb1735fb8dbee5dc38821915f23ba4
Pashka <64590900+wauv@users.noreply.github.com>
2023-09-04 14:39:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ wauv = commands.Bot(command_prefix="-",
                     intents=intents,
                     help_command=None)
 
-GUILD_ID = PASTE_YOUR_GUILD_ID
-CHANNEL_ID = PASTE_YOUR_CHANNEL_ID
+GUILD_ID = PASTE_YOUR_GUILD_ID     // Paste the Guild ID of the server
+CHANNEL_ID = PASTE_YOUR_CHANNEL_ID     // Paste the Channel ID of the channel
 
 
 @wauv.event
@@ -26,8 +26,8 @@ async def on_ready():
     print(f'Logged in as {wauv.user} ({wauv.user.id})')
     vc = discord.utils.get(wauv.get_guild(GUILD_ID).channels, id=CHANNEL_ID)
     await vc.guild.change_voice_state(channel=vc,
-                                      self_mute=False,
-                                      self_deaf=False)
+                                      self_mute=False,     // Toggle mute
+                                      self_deaf=False)     // Toggle deafen
     print(f"Successfully joined {vc.name} ({vc.id})")
 
 keep_alive()
